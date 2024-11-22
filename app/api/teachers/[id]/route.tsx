4fa51e6eb6aebe0a7451fc1571c559e130876de6
Steven Hutchison <hutchison.music@gmail.com>
2024-11-22 09:22:22 -0600

    Add authorization to teachers/id endpoint

--- app/api/teachers/[id]/route.tsx
+++ app/api/teachers/[id]/route.tsx
@@ -1,11 +1,18 @@
 import { apiResponse, User } from "@/app/types";
 import { sql } from "@vercel/postgres";
-import { NextResponse } from "next/server";
+import { NextRequest, NextResponse } from "next/server";
+import jwt from 'jsonwebtoken';
 
 export async function GET(
+    request: NextRequest,
     { params }: { params: Promise<{ id: string }> }
   ): apiResponse<User> {
     const id = (await params).id;
+    const token = request.cookies.get('token')?.value
+    if (!token) return NextResponse.json({message: 'not authorized'}, {status: 401})
+    const user = jwt.decode(token, {json: true});
+    if (!user || id != user.userId) return NextResponse.json({message: 'not authorized'}, {status: 401})
+    
     const res = await sql`SELECT * FROM teachers WHERE id = ${id}`;
     if (res.rowCount != 1) {
         return NextResponse.json({message: 'no teachers to return'}, {status: 200});
