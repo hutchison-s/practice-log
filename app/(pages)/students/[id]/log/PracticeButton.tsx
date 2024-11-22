c5a58890c76f33ded2d17c615653d616485a3f68
Steven Hutchison <hutchison.music@gmail.com>
2024-11-22 12:16:22 -0600

    Redesign journal entry form

--- app/(pages)/students/[id]/log/PracticeButton.tsx
+++ app/(pages)/students/[id]/log/PracticeButton.tsx
@@ -70,7 +70,7 @@ export default function PracticeButton({id}: {id: string}) {
                         Start Practice Session
                 </PrimaryButton>
         }
-        <dialog ref={modalRef} className="bg-transparent fixed">
+        <dialog ref={modalRef} className="size-full bg-transparent fixed max-w-[400px] max-h-[600px]">
             <form 
                 onSubmit={(e)=>{
                     e.preventDefault();
@@ -81,10 +81,10 @@ export default function PracticeButton({id}: {id: string}) {
                     e.currentTarget.reset();
                     router.refresh();
                 }}
-                className="w-[100vw] h-[100vh] max-w-[400px] max-h-[600px] grid gap-4 bg-background border-2 border-txtprimary shadow-lg rounded-lg p-8 text-txtprimary"
+                className="size-full grid gap-4 items-center justify-items-center bg-secondary border-2 border-txtprimary shadow-lg rounded-lg p-8 text-txtprimary"
             >
-                <label htmlFor="journal" className="w-full font-bold text-center">What did you practice?</label>
-                <textarea name="journal" id="journal" className="w-full h-full min-h-[300px] bg-black/50 rounded border-secondary border-[1px] resize-none p-3"></textarea>
+                <label htmlFor="journal" className="w-full font-bold text-center">What did you accomplish today?</label>
+                <textarea name="journal" id="journal" className="size-full min-h-[300px] bg-black/25 rounded border-txtsecondary border-[1px] resize-none p-3"></textarea>
                 <PrimaryButton size="md" type="submit" onClick={undefined}>Submit</PrimaryButton>
             </form>
         </dialog>
