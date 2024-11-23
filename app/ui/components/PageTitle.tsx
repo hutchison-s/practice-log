6a8ab40e318e13de50ee8a652f2913ae28a12d11
Steven Hutchison <hutchison.music@gmail.com>
2024-11-22 21:34:52 -0600

    add specific pixel breakpoints

--- app/ui/components/PageTitle.tsx
+++ app/ui/components/PageTitle.tsx
@@ -8,9 +8,9 @@ export default function PageTitle({
             {/* title text  */}
             <h1 className="z-20 text-3xl text-center px-4 font-bold md:text-5xl" style={{textShadow: "2px 2px var(--dark-slate)"}}>{children}</h1>
             {/* left circle */}
-            <div className="z-10 absolute top-0 left-0 -translate-x-1/2 size-[52px] md:size-[60px] aspect-square bg-lighter rounded-full border-8 border-secondary"></div>
+            <div className="z-10 absolute top-0 left-0 -translate-x-1/2 size-[52px] md:size-[64px] aspect-square bg-lighter rounded-full border-8 border-secondary"></div>
             {/* right circle */}
-            <div className="z-10 absolute top-0 right-0 translate-x-1/2 size-[52px] md:size-[60px] bg-secondary rounded-full outline outline-1 -outline-offset-8 outline-lighter"></div>
+            <div className="z-10 absolute top-0 right-0 translate-x-1/2 size-[52px] md:size-[64px] bg-secondary rounded-full outline outline-1 -outline-offset-8 outline-lighter"></div>
             
         </div>
     )
