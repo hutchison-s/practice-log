ddde8345c0f5885b95019827297f754d6bd83b44
Steven Hutchison <hutchison.music@gmail.com>
2024-12-19 08:56:40 -0600

    Allow custom classes for Body Text component

--- app/ui/components/BodyText.tsx
+++ app/ui/components/BodyText.tsx
@@ -1,10 +1,12 @@
 export default function BodyText({
     children,
+    className
   }: Readonly<{
-    children: React.ReactNode;
+    children: React.ReactNode,
+    className?: string
   }>) {
     return (
-        <p className="text-txtsecondary leading-relaxed mx-auto w-full max-w-[800px]">
+        <p className={"text-txtsecondary leading-relaxed mx-auto w-full max-w-[800px] "+className}>
             {children}
         </p>
     )
