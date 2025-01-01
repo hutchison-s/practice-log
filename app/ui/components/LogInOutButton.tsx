1af2c589ae1f6796f500c385265334401a99cfc9
Steven Hutchison <hutchison.music@gmail.com>
2025-01-01 16:41:49 -0600

    Add labels to mobile nav buttons

--- app/ui/components/LogInOutButton.tsx
+++ app/ui/components/LogInOutButton.tsx
@@ -5,7 +5,7 @@ import { LogIn, LogOut } from "lucide-react";
 import { useRouter } from "next/navigation";
 
 const mobileNavStyle =
-  "grid place-items-center glass text-xl rounded hover:bg-gradient-to-br hover:from-cyan-500 hover:to-teal-800 z-50 active:bg-indigo-950/50";
+  "flex flex-col justify-center items-center gap-1 glass text-xl rounded hover:bg-gradient-to-br hover:from-cyan-500 hover:to-teal-800 z-50 active:bg-indigo-950/50";
 
 export default function LogInOutButton({isMobile, closeMenu}: {isMobile?: boolean, closeMenu?: ()=>void}) {
     const {user, logout} = useUser();
@@ -27,8 +27,8 @@ export default function LogInOutButton({isMobile, closeMenu}: {isMobile?: boolea
     return (
         isMobile
         ? user.id != ''
-            ?   <div onClick={handleLogout} role="button" className={mobileNavStyle}><LogOut size={80}/></div>
-            :   <div onClick={handleLogin} role="button" className={mobileNavStyle}><LogIn size={80}/></div>
+            ?   <div onClick={handleLogout} role="button" className={mobileNavStyle}><LogOut size={80}/><span className="font-light text-sm font-inter text-zinc-400">Sign In</span></div>
+            :   <div onClick={handleLogin} role="button" className={mobileNavStyle}><LogIn size={80}/><span className="font-light text-sm font-inter text-zinc-400">Sign Out</span></div>
         : user.id != ''
             ?   <div onClick={handleLogout} role="button" className="p-2 rounded cursor-pointer my-auto hover:text-teal-500 text-txtprimary"><LogOut size={18} /></div>
             :   <div onClick={handleLogin} role="button" className="p-2 rounded cursor-pointer my-auto hover:text-teal-500 text-txtprimary"><LogIn size={18} /></div>
