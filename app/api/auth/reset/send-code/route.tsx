a5f82610250b1cccc1b17eefe43963d4ed48086c
Steven Hutchison <hutchison.music@gmail.com>
2025-01-04 17:24:11 -0600

    Change reset password request component to client component for status handling on same page

--- app/api/auth/reset/send-code/route.tsx
+++ app/api/auth/reset/send-code/route.tsx
@@ -3,9 +3,9 @@ import { sql } from "@vercel/postgres";
 import { NextRequest, NextResponse } from "next/server";
 
 export async function POST(request: NextRequest) {
-    const fd= await request.formData();
+    const fd = await request.formData();
     const email = fd.get('email') as string
-    if (!email) return NextResponse.redirect(new URL('/password-reset/failed?reason='+encodeURIComponent("Invalid or missing email"), request.url))
+    if (!email) return NextResponse.json({message:"Invalid or missing email"}, {status: 400})
     const {rows: teachers} = await sql`
         SELECT 
             name
@@ -14,7 +14,7 @@ export async function POST(request: NextRequest) {
         WHERE 
             email = ${email}
     `;
-    if (!teachers || teachers.length == 0) return NextResponse.redirect(new URL('/password-reset/failed?reason='+encodeURIComponent("User does not exist"), request.url))
+    if (!teachers || teachers.length == 0) return NextResponse.json({message:"User does not exist"}, {status: 403})
     await sendPasswordResetEmail({name: teachers[0].name, email: email});
-    return NextResponse.redirect(new URL('/password-reset/sent', request.url));
+    return NextResponse.json({message: 'Success'}, {status: 200});
 }
--- app/password-reset/page.tsx
+++ app/password-reset/page.tsx
@@ -1,16 +1,38 @@
-import React from 'react'
+'use client'
+
+import React, { FormEvent, useEffect, useState } from 'react'
 import SmallPageWrapper from '../ui/components/SmallPageWrapper'
 import PageTitle from '../ui/components/PageTitle'
 import GlassDiv from '../ui/components/GlassDiv'
 import { PrimaryButton } from '../ui/components/Buttons';
+import { CircleCheck } from 'lucide-react'
 
 function RequestReset() {
-    const apiURL = process.env.NEXT_PUBLIC_API_BASE_URL;
+    const [isSent, setIsSent] = useState(false);
+    const [hasError, setHasError] = useState('');
+
+    function handleSubmit(e: FormEvent<HTMLFormElement>) {
+        e.preventDefault();
+        fetch(`/api/auth/reset/send-code`, {method: 'POST', body: new FormData(e.currentTarget)})
+            .then(async (res) => {
+                if (!res.ok) {
+                    const {message} = await res.json();
+                    console.error(message)
+                    setHasError(message);
+                }
+                setIsSent(true);
+            }).catch(err => console.error(err))
+    }
+
+    useEffect(()=>{
+
+    }, [])
+
   return (
     <SmallPageWrapper>
         <PageTitle>Request Password Reset</PageTitle>
         <GlassDiv>
-            <form action={`${apiURL}/auth/reset/send-code`} method="post" className='mx-auto p-4 max-w-[500px] grid place-items-center gap-2 text-center'>
+            <form onSubmit={handleSubmit} className='mx-auto p-4 max-w-[500px] grid place-items-center gap-2 text-center'>
             <p>Enter the email address associated with your account and we will send you a link to reset your password.</p>
             <label className="relative my-8 block w-full rounded group">
         <span 
@@ -26,6 +48,8 @@ function RequestReset() {
             className='w-full p-2 border-2 rounded bg-transparent focus:outline-none focus:bg-background'
         />
     </label>
+    {hasError && <p className='text-sm text-center my-4'>{hasError}</p>}
+    {isSent && <p className='flex gap-2'><CircleCheck className='text-teal-500'/> Link has been sent to your email address</p>}
     <PrimaryButton type='submit' onClick={undefined}>Send Email</PrimaryButton>
             </form>
         </GlassDiv>
