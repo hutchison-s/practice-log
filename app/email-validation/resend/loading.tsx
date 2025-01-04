46c3e157535c806e5f6e40e460cba953b0e1ecc8
Steven Hutchison <hutchison.music@gmail.com>
2025-01-04 16:37:32 -0600

    Add suspense bounary to validation resend page

---
+++ app/email-validation/resend/loading.tsx
@@ -0,0 +1,15 @@
+import PageTitle from '@/app/ui/components/PageTitle'
+import SmallPageWrapper from '@/app/ui/components/SmallPageWrapper'
+import { Loader } from 'lucide-react'
+import React from 'react'
+
+function LoadingPage() {
+  return (
+    <SmallPageWrapper>
+        <PageTitle>Loading</PageTitle>
+        <Loader className='spinner' size={120}/>
+    </SmallPageWrapper>
+  )
+}
+
+export default LoadingPage
