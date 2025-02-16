13f01396c21158aa16176258f64de5374a4e4035
Steven Hutchison <hutchison.music@gmail.com>
2025-02-16 14:23:06 -0600

    Remove backdrop blur from library resource display to prevent nested blur issues in UI.

--- app/(pages)/teachers/[id]/library/_components/LibraryResourceDisplay.tsx
+++ app/(pages)/teachers/[id]/library/_components/LibraryResourceDisplay.tsx
@@ -10,7 +10,7 @@ function LibraryResourceDisplay({r}: {r: LibraryResource}) {
 
   return (
     
-        r && <li className="flex gap-4 justify-between align-items-center bg-gradient-to-tl from-indigo-950/75 to-indigo-800/75 border-[1px] border-white/25 backdrop-blur rounded p-2 w-full">
+        r && <li className="flex gap-4 justify-between align-items-center bg-gradient-to-tl from-indigo-950/75 to-indigo-800/75 border-[1px] border-white/25 rounded p-2 w-full">
             <div className="flex-1 flex align-items-center">
                 
                 <Link href={r.url} target="blank" className="w-full flex gap-2"><ResourceIcon t={r.type} /> {r.title}</Link>
