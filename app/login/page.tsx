46957123a41be15c9bb8235d88aa4643939fae6f
Steven Hutchison <stevenhutchisonkc@gmail.com>
2025-02-23 17:01:36 -0600

    Add suspense boundary and loader for Login Form now that it uses useSearchParams

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -4,6 +4,8 @@ import LoginForm from "../_ui_components/forms/LoginForm";
 import SmallPageWrapper from "../_ui_components/layout/SmallPageWrapper";
 import BodyText from "../_ui_components/layout/BodyText";
 import { Metadata } from "next";
+import { Suspense } from "react";
+import { Loader } from "lucide-react";
 
 export const metadata: Metadata = {
     title: "Sign In",
@@ -16,7 +18,9 @@ export default async function Page() {
         <>
             <SmallPageWrapper>
                 <PageTitle>Sign In</PageTitle>
-                <LoginForm />
+                <Suspense fallback={<Loader size={80} className="mx-auto my-4 text-teal-500"/>}>
+                    <LoginForm />
+                </Suspense>
                 <div className="text-center leading-6">
                     <p>Don&apos;t have an account yet?</p>
                     <Link href={'/signup'} className="text-lighter underline">Create an Account</Link>
